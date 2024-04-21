089ededcd70b61629a0220824ccfa7963faeac76
acgc <acgc@computer-engineering.fr>
2024-04-21 14:11:24 -0400

    add 0 and 1

---
+++ runqueries/partial/dfuds/backtracking/runPartialDfudsBacktracking.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
+  ./runqueries-$file-bfs-sorted.sh > ../../../outputs/partial/dfuds/backtracking/$file.txt
+done
