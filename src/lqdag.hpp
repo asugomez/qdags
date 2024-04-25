b0782c2360e38f441d3b7661a871bddd0b46ee28
acgc <acgc@computer-engineering.fr>
2024-04-25 19:41:32 -0400

    add two points for qdag in subquadtree

--- src/lqdag.hpp
+++ src/lqdag.hpp
@@ -30,7 +30,7 @@ struct quadtree_formula { // represents the output of the formula we are evaluat
 
 // represents a subtree of the quadtree
 struct subQuadtreeChild {
-    qdag *qdag;
+    ::qdag *qdag;
     uint16_t level; // the level of the quadtree_formula. 0 to start.
     uint64_t node; // absolute position in the bv[level] of the quadtree
     double value;// = NO_VALUE_LEAF;
