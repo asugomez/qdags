0efa4fe1a1e82562653ccfe723da1941a2a40c23
acgc <acgc@computer-engineering.fr>
2024-04-26 11:15:18 -0400

    fix script dir

--- runqueries/partial/dfuds/nonFixedQueue/compilePartialDfudsNonFixedQueue.sh
+++ runqueries/partial/dfuds/nonFixedQueue/compilePartialDfudsNonFixedQueue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-~
+script_dir=$(dirname "$BASH_SOURCE")
 # acces to this file:
 # chmod +x ./runqueries/partial/louds/backtracking/runqueries-j3-bfs-sorted.sh
 for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
