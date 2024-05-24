7d33a4bcdbba5b0672655b7199c4e706f8f0a68a
asugomez <asuncion.gomez@ug.uchile.cl>
2024-05-23 22:34:33 -0400

    run trad join script and graphs python

--- runqueries/all/runTraditionalJoin.sh
+++ runqueries/all/runTraditionalJoin.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 chmod a+x *.sh
 data_csv="../outputs/all/results.csv"
-echo "" > $data_csv
 
 echo "j3;j4;p2;p3;p4;s1;s2;s3;s4;t2;t3;t4;ti2;ti3;ti4;tr1;tr2">> $data_csv
-for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
-  echo "file: $file"
-  input_file="./runqueries-$file-bfs-sorted.sh"
+for k in 1 10 100 1000; do
+  # echo k
+  echo "k: $k"
+  printf "$k;" >> $data_csv
+  for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
+    echo "file: $file"
+    input_file="./runqueries-$file-bfs-sorted.sh"
+    output_file="./runqueries-$file-bfs-sorted-args.sh"
+    results_file="../outputs/all/$file-k$k.txt"
 
-  results_file="../outputs/all/$file.txt"
+    while IFS= read -r line || [ -n "$line" ]; do
+      modified_line="$line $k"
+      echo "$modified_line"
+    done < "$input_file" > "$output_file"
 
-  chmod +x $input_file
+    chmod +x $output_file
 
-  $input_file >> $results_file
-  # Calculate mean using awk
-  mean=$(awk '{ suma += $1 } END { print suma / NR }' "$results_file")
-  printf "$mean;" >> $data_csv
+    $output_file >> $results_file
+    # Calculate mean using awk
+    mean=$(awk '{ suma += $1 } END { print suma / NR }' "$results_file")
+    printf "$mean;" >> $data_csv
+  done
+  echo "" >> $data_csv
 done
-echo "" >> $data_csv
 
