5983e9a6b48842b8ea3bae82b6bd8631b595b825
asugomez <asuncion.gomez@ug.uchile.cl>
2024-08-28 15:06:29 -0400

    change name all to original

--- runqueries/original/compileTraditionalJoinM1.sh
+++ runqueries/original/compileTraditionalJoinM1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
-  g++ -std=c++11 -O3 -O0 -DNDEBUG -I ~/include -L ~/lib -lsdsl -ldivsufsort -ldivsufsort64 "../../queries/all/$file.cpp" -o $file
+  g++ -std=c++11 -O3 -O0 -DNDEBUG -I ~/include -L ~/lib -lsdsl -ldivsufsort -ldivsufsort64 "../../queries/original/$file.cpp" -o $file
 done
