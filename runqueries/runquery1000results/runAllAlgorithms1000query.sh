e7a4f1b6ea8e04376eb6f4b18146774fbd9b9772
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2024-11-14 15:45:01 -0300

    results 1000 results

--- runqueries/runquery1000results/runAllAlgorithms1000query.sh
+++ runqueries/runquery1000results/runAllAlgorithms1000query.sh
@@ -12,35 +12,35 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 #echo "Running partial louds backtracking"
 #cd "$script_dir/partial/louds/backtracking/"
 #./runPartialLoudsBacktracking.sh
-echo "Running partial louds optimalOrder"
-cd "$script_dir/partial/louds/optimalOrder/"
-./runPartialLoudsNonFixed.sh
+#echo "Running partial louds optimalOrder"
+#cd "$script_dir/partial/louds/optimalOrder/"
+#./runPartialLoudsNonFixed.sh
 
 ## dfuds
 #echo "Running partial dfuds backtracking"
 #cd "$script_dir/partial/dfuds/backtracking/"
 #./runPartialDfudsBacktracking.sh
-echo "Running partial dfuds optimalOrder"
-cd "$script_dir/partial/dfuds/optimalOrder/"
-./runPartialDfudsNonFixed.sh
+#echo "Running partial dfuds optimalOrder"
+#cd "$script_dir/partial/dfuds/optimalOrder/"
+#./runPartialDfudsNonFixed.sh
 
 
 # ranked
 ## louds
-#echo "Running ranked louds backtracking"
-#cd "$script_dir/ranked/louds/backtracking/"
-#./runRankedLoudsBacktracking.sh
-#echo "Running ranked louds optimalOrder"
-#cd "$script_dir/ranked/louds/optimalOrder/"
-#./runRankedLoudsNonFixed.sh
+echo "Running ranked louds backtracking"
+cd "$script_dir/ranked/louds/backtracking/"
+./runRankedLoudsBacktracking.sh
+echo "Running ranked louds optimalOrder"
+cd "$script_dir/ranked/louds/optimalOrder/"
+./runRankedLoudsNonFixed.sh
 
 ## dfuds
-#echo "Running ranked dfuds backtracking"
-#cd "$script_dir/ranked/dfuds/backtracking/"
-#./runRankedDfudsBacktracking.sh
-#echo "Running ranked dfuds optimalOrder"
-#cd "$script_dir/ranked/dfuds/optimalOrder/"
-#./runRankedDfudsNonFixed.sh
+echo "Running ranked dfuds backtracking"
+cd "$script_dir/ranked/dfuds/backtracking/"
+./runRankedDfudsBacktracking.sh
+echo "Running ranked dfuds optimalOrder"
+cd "$script_dir/ranked/dfuds/optimalOrder/"
+./runRankedDfudsNonFixed.sh
 
 
 # lazy qdags
