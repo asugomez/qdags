5cc9c49ebfcea604f45d39ad7cbebaf01a4c4281
asugomez <asuncion.gomez@ug.uchile.cl>
2024-12-10 20:05:03 -0300

    rm line 36 for t4 pattern, and delete flag sanitizer for lqdags

--- runqueries/lqdags/compileLazyJoin.sh
+++ runqueries/lqdags/compileLazyJoin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
-  g++ -std=c++17 -O3 -DNDEBUG -ldivsufsort -ldivsufsort64 -fsanitize=address,undefined,leak -D_GLIBCXX_DEBUG "../../queries/lqdags/$file.cpp" -lsdsl -o $file
+  g++ -std=c++17 -O3 -DNDEBUG -ldivsufsort -ldivsufsort64 "../../queries/lqdags/$file.cpp" -lsdsl -o $file
 done
