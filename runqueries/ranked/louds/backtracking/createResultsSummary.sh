ac11c342b144745b3c6ecb51f600ffbb58af0770
asugomez <asuncion.gomez@ug.uchile.cl>
2024-12-26 10:35:53 -0300

    script for nodes partial louds abck

--- runqueries/ranked/louds/backtracking/createResultsSummary.sh
+++ runqueries/ranked/louds/backtracking/createResultsSummary.sh
@@ -4,24 +4,44 @@
 # run tests for each type_fun and each k
 for type_fun in 0; do
   chmod a+x *.sh
-  data_csv="../../../outputs/ranked/louds/backtracking/results-f$type_fun-v2.csv"
+#  data_csv="../../../outputs/ranked/louds/backtracking/results-f$type_fun-v2.csv"
   # echo type fun
   echo "type_fun : $type_fun"
-  echo "k;j3;j4;p2;p3;p4;s1;s2;s3;s4;t2;t3;t4;ti2;ti3;ti4;tr1;tr2">> $data_csv
+#  echo "k;j3;j4;p2;p3;p4;s1;s2;s3;s4;t2;t3;t4;ti2;ti3;ti4;tr1;tr2">> $data_csv
   # echo >> ../../../outputs/partial/louds/backtracking/results.csv
-  for k in 1 10 100 1000; do
+  for k in 10; do #1 10 100 1000; do
     # echo k
     echo "k: $k"
-    printf "$k;" >> $data_csv
-    for file in j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
-        results_file="../../../outputs/ranked/louds/backtracking/$file-f$type_fun-k$k.txt"
+#    printf "$k;" >> $data_csv
+    for file in t4; do # j3 j4 p2 p3 p4 s1 s2 s3 s4 t2 t3 t4 ti2 ti3 ti4 tr1 tr2; do
+        results_file="../../../outputs/ranked/louds/backtracking/$file-f$type_fun-k$k-nodes.txt"
+
 
         chmod +x $results_file
 
+        # Calculate the mean of hexadecimal numbers
+        sum=0
+        count=0
+
+        while IFS= read -r hex_number || [ -n "$hex_number" ]; do
+          # Convert the hexadecimal number to decimal
+          decimal_value=$((0x$hex_number))
+          sum=$((sum + decimal_value))
+
+          count=$((count + 1))
+        done < "$results_file"
+
+        # Calculate mean and handle division by zero
+        if [ $count -gt 0 ]; then
+          mean=$((sum / count))
+        else
+          mean=0
+        fi
+
         # Calculate mean using awk
-        mean=$(awk '{ suma += $1 } END { print suma / NR }' "$results_file")
-        printf "$mean;" >> $data_csv
+#        mean=$(awk '{ suma += $1 } END { print suma / NR }' "$results_file")
+        printf "mean is: $mean;" # >> $data_csv
     done
-    echo "" >> $data_csv
+#    echo "" >> $data_csv
   done
 done
