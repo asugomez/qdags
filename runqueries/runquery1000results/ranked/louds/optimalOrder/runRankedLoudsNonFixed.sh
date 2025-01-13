d7bcda1c36da6d7774b202b5091bee2e860281b5
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2025-01-13 09:13:51 -0300

    script v1000 time ranked louds opt

--- runqueries/runquery1000results/ranked/louds/optimalOrder/runRankedLoudsNonFixed.sh
+++ runqueries/runquery1000results/ranked/louds/optimalOrder/runRankedLoudsNonFixed.sh
@@ -39,23 +39,23 @@ for type_fun in 0; do
       $output_file >> $results_file
 
       # Calculate the mean of hexadecimal numbers
-      sum=0
-      count=0
+      #sum=0
+      #count=0
 
-      while IFS= read -r hex_number || [ -n "$hex_number" ]; do
+      #while IFS= read -r hex_number || [ -n "$hex_number" ]; do
         # Convert the hexadecimal number to decimal
-        decimal_value=$((0x$hex_number))
-        sum=$((sum + decimal_value))
-        count=$((count + 1))
-      done < "$results_file"
+       # decimal_value=$((0x$hex_number))
+       # sum=$((sum + decimal_value))
+       # count=$((count + 1))
+      #done < "$results_file"
 
       # Calculate mean and handle division by zero
-      if [ $count -gt 0 ]; then
-        mean=$((sum / count))
-      else
-        mean=0
-      fi
-
+      #if [ $count -gt 0 ]; then
+      #  mean=$((sum / count))
+      #else
+      #  mean=0
+      #fi
+      mean=$(awk '{ suma += $(('0'x1)) } END { print suma / NR }' "$results_file")
       printf "$mean;" >> $data_csv
     done
     echo "" >> $data_csv
