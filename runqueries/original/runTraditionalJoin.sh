e6912f2e73a0616bd60d569ba3121a25f9124294
asugomez <asuncion.gomez@ug.uchile.cl>
2025-05-16 08:48:52 -0400

    [SCRIPT][ORIGINAL] change runOriginal for count nodes

--- runqueries/original/runTraditionalJoin.sh
+++ runqueries/original/runTraditionalJoin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod a+x *.sh
-data_csv="../outputs/original/results-time.csv"
+data_csv="../outputs/original/results-nodes.csv"
 
 echo "k;j3;j4;p2;p3;p4;s1;s2;s3;s4;t2;t3;t4;ti2;ti3;ti4;tr1;tr2">> $data_csv
 for k in 1 10 100 1000; do #
@@ -11,7 +11,7 @@ for k in 1 10 100 1000; do #
     echo "file: $file"
     input_file="./runqueries-$file-bfs-sorted.sh"
     output_file="./runqueries-$file-bfs-sorted-args.sh"
-    results_file="../outputs/original/$file-k$k-time-res.txt"
+    results_file="../outputs/original/$file-k$k-nodes.txt"
 
     while IFS= read -r line || [ -n "$line" ]; do
       if [[ "$line" =~ [[:space:]]$ ]]; then
@@ -27,26 +27,26 @@ for k in 1 10 100 1000; do #
 
     # OPTION 1: for counting nodes
     # Calculate the mean of hexadecimal numbers
-#    sum=0
-#    count=0
-#
-#    while IFS= read -r hex_number || [ -n "$hex_number" ]; do
-#      # Convert the hexadecimal number to decimal
-#      decimal_value=$((0x$hex_number))
-#      sum=$((sum + decimal_value))
-#      count=$((count + 1))
-#    done < "$results_file"
-#
-#    # Calculate mean and handle division by zero
-#    if [ $count -gt 0 ]; then
-#      mean=$((sum / count))
-#    else
-#      mean=0
-#    fi
+    sum=0
+    count=0
+
+    while IFS= read -r hex_number || [ -n "$hex_number" ]; do
+      # Convert the hexadecimal number to decimal
+      decimal_value=$((0x$hex_number))
+      sum=$((sum + decimal_value))
+      count=$((count + 1))
+    done < "$results_file"
+
+    # Calculate mean and handle division by zero
+    if [ $count -gt 0 ]; then
+      mean=$((sum / count))
+    else
+      mean=0
+    fi
 
     # OPTION 2: for execution time
     # Calculate mean using awk
-    mean=$(awk '{ suma += $(('0'x1)) } END { print suma / NR }' "$results_file")
+#    mean=$(awk '{ suma += $(('0'x1)) } END { print suma / NR }' "$results_file")
     printf "$mean;" >> $data_csv
   done
   echo "" >> $data_csv
