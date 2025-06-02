7cee2eab1448963e00355de2cfc6fb3377e0c529
Asuncion Gomez <asuncion.gomez@ug.uchile.cl>
2025-06-02 14:07:41 -0400

    [RUNQUEIRES][LQDAGS][V1000] comment those lines killed

--- runqueries/runquery1000results/lqdags/runqueries-p4-bfs-sorted.sh
+++ runqueries/runquery1000results/lqdags/runqueries-p4-bfs-sorted.sh
@@ -1,3 +1,3 @@
-./p4 ../../../data/all/prop-direct-P706 ../../../data/all/prop-direct-P488 ../../../data/all/prop-direct-P937 ../../../data/all/prop-direct-P2959
+#./p4 ../../../data/all/prop-direct-P706 ../../../data/all/prop-direct-P488 ../../../data/all/prop-direct-P937 ../../../data/all/prop-direct-P2959
 ./p4 ../../../data/all/prop-direct-P1532 ../../../data/all/prop-direct-P460 ../../../data/all/prop-direct-P706 ../../../data/all/prop-direct-P237
-./p4 ../../../data/all/prop-direct-P680 ../../../data/all/prop-direct-P2293 ../../../data/all/prop-direct-P1995 ../../../data/all/prop-direct-normalized-P508
+#./p4 ../../../data/all/prop-direct-P680 ../../../data/all/prop-direct-P2293 ../../../data/all/prop-direct-P1995 ../../../data/all/prop-direct-normalized-P508
--- runqueries/runquery1000results/lqdags/runqueries-t3-bfs-sorted.sh
+++ runqueries/runquery1000results/lqdags/runqueries-t3-bfs-sorted.sh
@@ -1,1 +1,1 @@
-./t3 ../../../data/all/prop-direct-P4728 ../../../data/all/prop-direct-P961 ../../../data/all/prop-direct-P5984
+#./t3 ../../../data/all/prop-direct-P4728 ../../../data/all/prop-direct-P961 ../../../data/all/prop-direct-P5984
